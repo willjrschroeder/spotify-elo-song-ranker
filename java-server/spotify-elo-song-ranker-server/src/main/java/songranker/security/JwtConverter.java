9494b0c1a324ed858266f73657b9b5f7ed79d334
BSimmons10 <simmonsbgup@gmail.com>
2022-10-10 14:33:12 -0500

    partial security update

---
+++ java-server/spotify-elo-song-ranker-server/src/main/java/songranker/security/JwtConverter.java
@@ -0,0 +1,21 @@
+package songranker.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtConverter {
+
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+
+}
