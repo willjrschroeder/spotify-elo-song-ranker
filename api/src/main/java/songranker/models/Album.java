d2c0714374f9766ac7932734bba678a0399069b3
unknown <willjrschroeder@gmail.com>
2022-10-10 13:53:24 -0500

    added album model

--- api/src/main/java/songranker/models/Album.java
+++ api/src/main/java/songranker/models/Album.java
@@ -1,4 +1,51 @@
 package songranker.models;
 
+import java.time.LocalDate;
+
 public class Album {
+    private String albumUri;
+    private String albumName;
+    private LocalDate releaseDate;
+    private String albumImageLink;
+    private String spotifyUrl;
+
+    public String getAlbumUri() {
+        return albumUri;
+    }
+
+    public void setAlbumUri(String albumUri) {
+        this.albumUri = albumUri;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getAlbumImageLink() {
+        return albumImageLink;
+    }
+
+    public void setAlbumImageLink(String albumImageLink) {
+        this.albumImageLink = albumImageLink;
+    }
+
+    public String getSpotifyUrl() {
+        return spotifyUrl;
+    }
+
+    public void setSpotifyUrl(String spotifyUrl) {
+        this.spotifyUrl = spotifyUrl;
+    }
 }
