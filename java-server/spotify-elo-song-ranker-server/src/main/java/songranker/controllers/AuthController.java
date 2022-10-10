3a3dc1be843d564a7a13adb3f4310d4dc8f4ffe3
unknown <willjrschroeder@gmail.com>
2022-10-10 12:01:54 -0500

    added return statement

--- java-server/spotify-elo-song-ranker-server/src/main/java/songranker/controllers/AuthController.java
+++ java-server/spotify-elo-song-ranker-server/src/main/java/songranker/controllers/AuthController.java
@@ -46,5 +46,6 @@ public class AuthController {
         } catch (AuthenticationException ex) {
             System.out.println(ex);
         }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
 }
