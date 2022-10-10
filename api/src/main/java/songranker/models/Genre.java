86ed93180d2936878b66bb7f4427d4e301146bbe
unknown <willjrschroeder@gmail.com>
2022-10-10 13:51:07 -0500

    added genre model

--- api/src/main/java/songranker/models/Genre.java
+++ api/src/main/java/songranker/models/Genre.java
@@ -1,4 +1,22 @@
 package songranker.models;
 
 public class Genre {
+    private int genreId;
+    private String genreName;
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
 }
