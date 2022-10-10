280b763c322669d01c6b6a0fc22c00f37d7c5829
BSimmons10 <simmonsbgup@gmail.com>
2022-10-10 16:06:00 -0500

    update

--- api/src/main/java/songranker/models/AppUser.java
+++ api/src/main/java/songranker/models/AppUser.java
@@ -1,2 +1,55 @@
 package songranker.models;
 
+
+public class AppUser {
+
+    private int appUserId;
+
+    private String username;
+
+    private String passwordHash;
+
+    private String displayName;
+
+    private boolean disabled;
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+}
