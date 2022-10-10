0e8a82031d15a7b0288e485f127f3cc05fa96245
unknown <willjrschroeder@gmail.com>
2022-10-10 13:39:12 -0500

    added Track model (w/o relationships represented)

--- api/src/main/java/songranker/models/Track.java
+++ api/src/main/java/songranker/models/Track.java
@@ -1,4 +1,12 @@
 package songranker.models;
 
 public class Track {
+    private String track_uri;
+    private String title;
+    private int eloScore;
+    private int numOfMatchesPlayed; // number of times the song has been in an ELO match
+    private int trackDuration; // stored in milliseconds
+    private int popularityNum; // Spotify's popularity of the track. Based on number of recent streams
+    private String spotifyUrl; // link to the full track on Spotify's service
+    private String previewUrl; // link to a 30-second track preview in .mp3 format
 }
