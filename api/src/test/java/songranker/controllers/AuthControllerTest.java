6e952df3653387931d4108660127c982a5e284b4
unknown <willjrschroeder@gmail.com>
2022-10-10 15:23:22 -0500

    added test app properties file and AuthControllerTest skeleton

---
+++ api/src/test/java/songranker/controllers/AuthControllerTest.java
@@ -0,0 +1,12 @@
+package songranker.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+}
