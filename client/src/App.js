01f66fd0d1a5bdd2364f8f8ef59764e55aa7788b
Jasper <jskinnersloan@gmail.com>
2022-10-10 14:33:20 -0500

    added context folder and UserContext

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,29 @@ import Home from './Home/Home';
 import React from 'react';
 import Register from './Register/Register'
 import Login from './Login/Login'
+import AuthContext from './AuthContext';
 
 function App() {
+const [loginInfo, setLoginInfo] = useState( null );
+
   return (
-
+    <div className='App'>
+    <AuthContext.Provider value = {loginInfo}>
       <BrowserRouter>
-        <Switch>
-          <Route exact path = "/">
-            <Home/>
-          </Route>
-          <Route exact path = "/login">
-            <Login></Login>
-          </Route>
-          <Route exact path = "/register">
-            <Register></Register>
-          </Route>
-        </Switch>
-
+          <Switch>
+            <Route exact path = "/">
+              <Home/>
+            </Route>
+            <Route exact path = "/login">
+              <Login></Login>
+            </Route>
+            <Route exact path = "/register">
+              <Register></Register>
+            </Route>
+          </Switch>
       </BrowserRouter>
+    </AuthContext.Provider>
+    </div>
   );
 }
 
---
+++ client/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// Initialize an empty context that will get a value later on
+const User = createContext(null);
+
+export default User;
