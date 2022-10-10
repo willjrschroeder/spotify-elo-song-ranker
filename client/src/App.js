452cade5fc8fae72159f5113a4380e1af3133d96
Jasper <jskinnersloan@gmail.com>
2022-10-10 15:14:07 -0500

    login and logout functions

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,46 @@ import Home from './Home/Home';
 import React from 'react';
 import Register from './Register/Register'
 import Login from './Login/Login'
-import AuthContext from './AuthContext';
+import AuthContext from './context/AuthContext';
+import jwtDecode from "jwt-decode";
 
 function App() {
-const [loginInfo, setLoginInfo] = useState( null );
+
+const [user, setUser] = useState( null );
+
+const login = (token) => {
+  const { sub: username, authorities : authoritiesString } = jwtDecode(token);
+
+  const roles = authoritiesString.split(',');
+
+  const user = {
+    username,
+    roles,
+    token,
+    hasRole(role) {
+      return this.roles.unclues(role);
+    }
+  };
+
+  //for debugging
+  console.log(user);
+  
+  setUser(user);
+
+  return user;
+};
+
+const logout = () => {
+  setUser(null);
+};
+
+const auth = {
+  user: user ? {...user} : null,
+  login,
+  logout
+};
+
+
 
   return (
     <div className='App'>
