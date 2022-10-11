49c8102ddc45928cdbc4fe8ed3e7db35da5c2a16
BSimmons10 <simmonsbgup@gmail.com>
2022-10-11 11:36:26 -0500

    testing update

--- api/src/test/java/songranker/data/AppUserJdbcRepoTest.java
+++ api/src/test/java/songranker/data/AppUserJdbcRepoTest.java
@@ -1,10 +1,27 @@
 package songranker.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import songranker.data.mappers.AppUserJdbcRepo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class AppUserJdbcRepoTest {
+
+    @Autowired
+    AppUserJdbcRepo repo;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup(){
+        knownGoodState.set();
+    }
 
     @Test
     void getUserByUsername() {
