b301b43977b458efd7b0c722dbafb6e726e0ec21
unknown <willjrschroeder@gmail.com>
2022-10-11 10:01:37 -0500

    add UserDetailsService

--- api/src/main/java/songranker/security/UserDetailsServiceImplementation.java
+++ api/src/main/java/songranker/security/UserDetailsServiceImplementation.java
@@ -1,4 +1,26 @@
 package songranker.security;
 
-public class UserDetailsServiceImplementation {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import songranker.data.mappers.AppUserRepo;
+import songranker.models.AppUser;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+    @Autowired
+    AppUserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = repo.getUserByUsername( username );
+
+        if ( user == null ) {
+            throw new UsernameNotFoundException("Could not find user " + username);
+        }
+
+        return user;
+    }
 }
