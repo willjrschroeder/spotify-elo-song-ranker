56886985851e9468d75d5ab0fd1c80104516fe89
BSimmons10 <simmonsbgup@gmail.com>
2022-10-11 11:52:20 -0500

    Tests Update

--- api/src/test/java/songranker/data/AppRoleJdbcRepoTest.java
+++ api/src/test/java/songranker/data/AppRoleJdbcRepoTest.java
@@ -1,10 +1,27 @@
 package songranker.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import songranker.data.mappers.AppRoleJdbcRepo;
+import songranker.data.mappers.AppUserJdbcRepo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class AppRoleJdbcRepoTest {
+
+    @Autowired
+    AppRoleJdbcRepo repo;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup(){
+        knownGoodState.set();
+    }
 
     @Test
     void getRolesByUsername() {
