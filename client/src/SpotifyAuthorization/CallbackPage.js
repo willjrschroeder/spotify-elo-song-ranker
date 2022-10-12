c2f9bbcb15f6d3793034eb7700f01cecd3e17320
unknown <willjrschroeder@gmail.com>
2022-10-11 22:56:43 -0500

    documentation notes

--- client/src/SpotifyAuthorization/CallbackPage.js
+++ client/src/SpotifyAuthorization/CallbackPage.js
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 const code = new URLSearchParams(window.location.search).get('code');
 
 function CallbackPage() {
-    const spotifyAccessToken = useAuth(code);
+    const spotifyAccessToken = useAuth(code); //TODO: incorporate this token into React context
     return (
         <div>
             {spotifyAccessToken}
