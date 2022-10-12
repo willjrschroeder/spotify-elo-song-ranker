84d6db955c4a493c4aa15f97f9046f138783aea1
unknown <willjrschroeder@gmail.com>
2022-10-11 22:24:38 -0500

    working on spotify oauth

--- client/src/SpotifyAuthorization/useAuth.js
+++ client/src/SpotifyAuthorization/useAuth.js
@@ -1,18 +1,52 @@
 import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 function useAuth(code) {
     const [accessToken, setAccessToken] = useState();
     const [refreshToken, setRefreshToken] = useState();
     const [expiresIn, setExpiresIn] = useState();
+    const history = useHistory();
 
+    // this method is run once every time a new authorization code is generated.
+    // it makes a post request to the express server and is returned an accessToken, refreshToken, and expiration time
     useEffect(() => {
-        axios.post('http://localhost:3001/spotify_login', { // makes a post request to our express server, passing in the authorization code
+        axios.post('http://localhost:3001/api/spotify/login', { // makes a post request to our express server, passing in the authorization code
             code
-        }).then(response => {
+        }).then(response => { // this response contains our access_token and our refresh_token
             console.log(response.data);
+            setAccessToken(response.data.accessToken);
+            setRefreshToken(response.data.refreshToken);
+            setExpiresIn(response.data.expiresIn);
+
+            window.history.pushState({}, null, "/"); // This removes the auth code from the URL. Doesn't do anything functionally, just makes things look a little cleaner
+        }).catch(() => { // if there is an error during code authentication, the user is pushed back to the spotify OAuth page.
+            // TODO: Not sure if we want to handle this error in another way
+            // **THIS currently happens every time. This is due to React making two requests on every page load
+            history.push("/spotify_login");
         })
     }, [code]); // performs every time a new authentication code is accquired
+
+
+    // this method is run every time the refreshToken or expiresIn variable is updated
+    // it makes a post request to the express server and is returned a new access token with a new expiration time
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return; // This makes sure a refresh request is not being made before these fields are defined
+
+        axios.post('http://localhost:3001/api/spotify/refresh_token', { // makes a post request to our express server, passing in the refreshToken
+            refreshToken
+        }).then(response => { // this response contains our access_token and our refresh_token
+            console.log(response.data);
+            setAccessToken(response.data.accessToken);
+            setExpiresIn(response.data.expiresIn);
+        }).catch(() => { // if there is an error during code authentication, the user is pushed back to the spotify OAuth page.
+            // TODO: Not sure if we want to handle this error in another way
+            // **THIS currently happens every time. This is due to React making two requests on every page load
+            history.push("/spotify_login");
+        })
+    }, [refreshToken, expiresIn])
+
+    return accessToken; // returns access token to the callback page
 }
 
 export default useAuth;
--- express_server/server.js
+++ express_server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node'); // This is a library for accessing the Spotify API. Simplifies all requests
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -8,7 +8,7 @@ app.use(cors()); // gets rid of cors error for accessing from localhost:3000
 app.use(bodyParser.json()); // allows request body to be parsed
 
 // this creates an API endpoint for getting a spotify access token from an authorization code
-app.post('/spotify_login', (request, response) => {
+app.post('/api/spotify/login', (request, response) => {
     const code = request.body.code;
     const spotifyApi = new SpotifyWebApi({
         redirectUri: 'http://localhost:3000/callback',
@@ -18,14 +18,39 @@ app.post('/spotify_login', (request, response) => {
 
     spotifyApi.authorizationCodeGrant(code)
     .then(data => {
-        response.json({
+        response.json({ // set the response body to contain auth token details
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         })
-    }).catch(() => {
+    }).catch((err) => {
+        console.log(err);
         response.sendStatus(400); // error that is sent when code cannot get access token
     })
-})
+});
 
-app.listen(3001); // This servers is listening on port 3001
+// this creates an API endpoint for refreshing a spotify access token
+app.post('/api/spotify/refresh_token', (request, response) => {
+    const refreshToken = request.body.refreshToken;
+
+    //builds a spotify API instance containing the refresh token (needed to generate a fresh token)
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000/callback',
+        clientId: 'b055b73f53474f3e931fd58a080ca3cf',
+        clientSecret: '7b90f4b463e84c6186e70402c1d29159',
+        refreshToken
+    });
+
+    // makes a request to the spotify API to refresh the access token
+    spotifyApi.refreshAccessToken().then( (data) => {
+        response.json({ // set the response body to contain auth token details
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch((err) => {
+        console.log(err);
+        response.sendStatus(400); // error that is sent when code cannot get access token
+    })
+});
+
+app.listen(3001); // The express server is listening on port 3001
