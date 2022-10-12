285d46a31e7859eba48dcb2390afad4ccf27f5e9
unknown <willjrschroeder@gmail.com>
2022-10-12 09:41:49 -0500

    updated register form

--- client/src/Register/Register.js
+++ client/src/Register/Register.js
@@ -1,14 +1,16 @@
 import FormInput from "../FormInput/FormInput";
-import {useState, useContext} from "react";
+import { useState, useContext } from "react";
 import AuthContext from "../context/AuthContext";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Register() {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
     const [errors, setErrors] = useState([]);
-    const loginRequest = {username, password};
+    const loginRequest = { username, password, displayName };
 
     const auth = useContext(AuthContext);
 
@@ -16,44 +18,60 @@ function Register() {
 
 
 
-function addUser( event ) {
-    event.preventDefault();
+    function addUser(event) {
+        event.preventDefault();
 
-    fetch( "http://localhost:8080/api/agent", {
-        method:"POST",
-        body: JSON.stringify(loginRequest),
-        headers: {
-            "Content-Type": "application/json"
+        if(password !== confirmPassword) {
+            setErrors(...errors, ["Password and confirm password do not match"]);
+            return;
+            //TODO: display this error
         }
-    }).then( async response => {
-        if( response.status === 201 ) {
-            history.push("/");
-            return response.json();
-        } 
-            return Promise.reject( await response.json() );
-    })
-    .catch( errorList => {
-        if( errorList instanceof TypeError ){
-            console.log( "Could not connect to api.");
-        } else {
-            console.log(errorList);
-        }
-    });
-}
 
-    
-    return(<>
+        fetch("http://localhost:8080", {
+            method: "POST",
+            body: JSON.stringify(loginRequest),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(async response => {
+            if (response.status === 201) {
+                history.push("/");
+                return response.json();
+            }
+            return Promise.reject(await response.json());
+        })
+            .catch(errorList => {
+                if (errorList instanceof TypeError) {
+                    console.log("Could not connect to api.");
+                } else {
+                    console.log(errorList);
+                }
+            });
+    }
+
+
+    return (<>
         <form onSubmit={addUser}>
             <FormInput
-                InputType = {"text"}
-                indentifier = {"username"}
-                labelText = {"Username"}
-                onChangeHandler = {(event) => setUsername(event.target.value)}/>
+                InputType={"text"}
+                indentifier={"username"}
+                labelText={"Username"}
+                onChangeHandler={(event) => setUsername(event.target.value)} />
             <FormInput
-                InputType = {"password"}
-                indentifier = {"password"}
-                labelText = {"Password"}
-                onChangeHandler = {(event) => setPassword(event.target.value)}/>
+                InputType={"password"}
+                indentifier={"password"}
+                labelText={"Password"}
+                onChangeHandler={(event) => setPassword(event.target.value)} />
+            <FormInput
+                InputType={"password"}
+                indentifier={"confirmPassword"}
+                labelText={"Confirm Password"}
+                onChangeHandler={(event) => setConfirmPassword(event.target.value)} />
+            <FormInput
+                InputType={"text"}
+                indentifier={"displayName"}
+                labelText={"Display Name"}
+                onChangeHandler={(event) => setDisplayName(event.target.value)} />
         </form>
     </>
     )
