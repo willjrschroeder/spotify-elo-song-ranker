19e63c4e6115692bd18634888ebe3473256be98c
Jasper <jskinnersloan@gmail.com>
2022-10-14 15:18:14 -0500

    working on tracks

--- client/src/ManagePlaylists/ManagePlaylists.js
+++ client/src/ManagePlaylists/ManagePlaylists.js
@@ -1,9 +1,8 @@
 import Playlist from "../Playlist/Playlist"
-import PlaylistNames from "../Playlist/PlaylistNames/PlaylistNames"
 import "./ManagePlaylists.css"
 import SpotifyWebApi from 'spotify-web-api-node';
 import SpotifyAuthContext from '../context/SpotifyAuthContext';
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 
 
@@ -11,6 +10,7 @@ const spotifyApi = new SpotifyWebApi({
     clientId: 'b055b73f53474f3e931fd58a080ca3cf'
 });
 function ManagePlaylists() {
+
     const spotifyAuth = useContext(SpotifyAuthContext); 
 
     const [playlists, setPlaylists] = useState([]);
@@ -65,29 +65,19 @@ function ManagePlaylists() {
     }
     
         return(<>
-        <Link to="/home">Home</Link>
-        <div className="">
-            {playlists.map( (p, index) => <Playlist key={p.id} playlistData={p} index={index}  />) }
+        <Link to="/home" className="login">Home</Link>
+        <div className="container">
+            {playlists.map( (p, index)=> (
+                <div key={index}>
+                    <img className="images" src={p.images[0].url}></img>
+                    <button onClick={addPlaylistToDatabase}>Add Tracks</button>
+                </div>
+                
+                )
+            )}
         </div>
-        {/* <div className="player">
-            <div class="upper-part">
-                <div class="play-icon">
-                    <svg width="20" height="20" fill="#2992dc" stroke="#2992dc" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-play" viewBox="0 0 24 24">
-                    <defs/>
-                    <path d="M5 3l14 9-14 9V3z"/>
-                    </svg>
-                </div>
-                <div className="info-area" id="test">
-                    {playlists.map( (pl, index) => <PlaylistNames key={pl.id} playlistData={pl} onPlaylistAdd={addPlaylistToDatabase} index={index}  />)}
-                </div>
-            </div>
-            <div class="progress-bar">
-                <span class="progress"></span>
-            </div>
-        </div> */}
         </>
         )
-    
     }
 
 
--- client/src/Playlist/Playlist.js
+++ client/src/Playlist/Playlist.js
@@ -2,18 +2,10 @@ import "./Playlist.css";
 function Playlist(props) {
 
     return(<>
-    <div className="container">
-        <div className="cards">
-            <label className="card">
-                <img src={props.playlistData.images[0].url} alt="Playlist"></img>
-            </label>
-        </div>
-        <label className="song-info" id="song-info-1">
-            <div className="title">{props.playlistData.name}</div>
-            <div className="subtitle"></div>
+    <div className="card">
+        <h3>{props.playlistData.name}</h3>
+        <img src={props.playlistData.images[0]} alt="playlist image"></img>
             <button onClick={props.addPlaylistToDatabase}>Add all tracks from this playlist to rank</button>
-            
-        </label>
     </div>
     </>
     )
