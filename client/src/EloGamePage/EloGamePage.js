be0f2a7ad80ebf625c62182898832af8b206d0b6
Jasper <jskinnersloan@gmail.com>
2022-10-15 19:13:13 -0500

    a bit of refactoring

--- client/src/EloGamePage/EloGamePage.js
+++ client/src/EloGamePage/EloGamePage.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import useAuth from "../SpotifyAuthorization/useAuth";
-import GameDisplay from "./GameDisplay";
+import { useParams } from "react-router-dom";
 
-function EloGamePage(playlistId) {
-    const serverAuth = useAuth();
+function EloGamePage() {
 
+    const { playlistId } = useParams
     const [playlistTracks, setPlaylistTracks] = useState([]);
 
-    function loadTracksByPlaylists(playlistId) { //TODO: place this in a useEffect by itself - just want this to happen once
-            fetch( `http://localhost:8080/api/track/${playlistId}/${serverAuth.user.id}` )
+    function loadTracksByPlaylists(playlistId) {
+            fetch( "http://localhost:8080/api/tracks/" + playlistId )
             .then( response => {
                 if( response.status === 200 ) {
                     return response.json();
@@ -19,28 +18,13 @@ function EloGamePage(playlistId) {
             });
     }
 
-    function displayRandomTracks() {
-        //TODO: call on page load and everytime after a winner is chosen - probably a use effect (maybe update track scores goes in here too?)
-        //TODO: tracks that are currently awaiting a response from the Java server should be made unselectable somehow
-        //TODO: when the response from the server resolves, make the tracks selectable again
-    }
 
-    function updateTrackScores(winningTrack, losingTrack){ // takes an input of tracks with updated ELO scores
-        //TODO: make a post request to the server and await the response
-
-    }
 
 
     return(
         <>
         <h1>Rank your songs</h1>
 
-        <div>
-            <GameDisplay updateTrackScores={updateTrackScores} track1={null} track2={null}/> {/* GameDisplay is the two tracks combined, along with a mechanism to choose the winner */}
-        </div>
-        <div>
-            
-        </div>
         </>
     )
 }
