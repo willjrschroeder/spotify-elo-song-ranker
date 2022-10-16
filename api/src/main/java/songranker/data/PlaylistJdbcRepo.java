08483305897eb6537805dd4a2d1e6e6108f543e4
BSimmons10 <simmonsbgup@gmail.com>
2022-10-15 19:20:00 -0500

    repo updates

--- api/src/main/java/songranker/data/PlaylistJdbcRepo.java
+++ api/src/main/java/songranker/data/PlaylistJdbcRepo.java
@@ -1,6 +1,9 @@
 package songranker.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import songranker.data.mappers.PlaylistMapper;
 import songranker.models.Playlist;
 
 import java.util.List;
@@ -8,8 +11,13 @@ import java.util.List;
 
 @Repository
 public class PlaylistJdbcRepo implements PlaylistRepo {
+
+    @Autowired
+    JdbcTemplate template;
     @Override
     public List<Playlist> getPlaylistsByAppUserId(int appUserId) {
-        throw new UnsupportedOperationException();
+        final String sql = "select * from playlist where app_user_id = ?";
+
+        return template.query(sql, new PlaylistMapper(), appUserId);
     }
 }
