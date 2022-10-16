5d693c77c1145a3e321ea28256872ba29b533e34
Jasper <jskinnersloan@gmail.com>
2022-10-15 19:18:12 -0500

    added some basic code for display

--- client/src/EloGamePage/GameMechanism/GameTrack.js
+++ client/src/EloGamePage/GameMechanism/GameTrack.js
@@ -6,6 +6,12 @@ function GameTrack({ track1, track2}){
     
     return (
         <>
+        <div className="flexTrackUpdate">
+            <h3>{track1.title}: {track1.eloScore}</h3>
+        </div>
+        <div className="flexTrackUpdate">
+            <h3>{track2.title}: {track2.eloScore}</h3>
+        </div>
 
         </>
     );
