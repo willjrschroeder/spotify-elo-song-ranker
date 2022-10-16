d145b8999799cbee6260529780739d2134887b5d
BSimmons10 <simmonsbgup@gmail.com>
2022-10-15 19:16:14 -0500

    further validation repo updates

--- api/src/main/java/songranker/data/PlaylistJdbcRepo.java
+++ api/src/main/java/songranker/data/PlaylistJdbcRepo.java
@@ -1,9 +1,12 @@
 package songranker.data;
 
+import org.springframework.stereotype.Repository;
 import songranker.models.Playlist;
 
 import java.util.List;
 
+
+@Repository
 public class PlaylistJdbcRepo implements PlaylistRepo {
     @Override
     public List<Playlist> getPlaylistsByAppUserId(int appUserId) {
