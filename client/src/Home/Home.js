b7195be455a5cc371947d3ba59f1c59a3a070290
Jasper <jskinnersloan@gmail.com>
2022-10-18 16:00:47 -0500

    update

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,21 +1,65 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import AuthContext from "../context/AuthContext";
 import {Link} from "react-router-dom";
 import "./Home.css"
+import UserPlaylist from "./UserPlaylist";
 
 function Home() {
 
-
+    const [selectedPlaylist, setSelectedPlaylist] = useState();
+    const [playlists, setPlaylists] = useState([]);
+    const [tracks, setTracks] = useState([]);
     const auth = useContext(AuthContext);
 
-    function loadAllTracks() {
+    function loadPlaylistsbyUser() {
+        fetch( "http://localhost:8080/api/playlist/"+auth.user.id , {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + auth.user.token
+            }
+        })
+        .then( response => {
+            if( response.status === 200 ) {
+                return response.json();
+            } else( console.log( response ) )
+        } )
+        .then( playlists => {
+            console.log(playlists);
+            setPlaylists( playlists );
+        });
+    }
 
+    function loadTracksByPlaylist() {
+        fetch( "http://localhost:8080/api/track/playlist/" + selectedPlaylist , {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + auth.user.token
+            }
+        })
+        .then( response => {
+            if( response.status === 200 ) {
+                return response.json();
+            } else( console.log( response ) )
+        } )
+        .then( tracks => {
+            console.log(tracks);
+            setTracks( tracks );
+        });
     }
     useEffect(
         () => {
-            loadAllTracks();
+            loadPlaylistsbyUser();
         },[]
     )
+    function handleChange(event) {
+        let playlistId = event.target.value;
+        setSelectedPlaylist(playlistId);
+        
+        loadTracksByPlaylist();
+    }
+
     return (
         <>
             <div className="header">Home Page</div>
@@ -23,6 +67,20 @@ function Home() {
                 <Link to="/playlistManager" ><button>Manage Playlists</button></Link>
             </div>
             <div className="flex-summary">
+            <table className="table">
+                        <thead className="thead-dark">
+                            <tr className="table-rows">
+                                <th colSpan={2}>Spotify Playlists</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {playlists.map((playlist, index) => {
+                                const databasePlaylistUris = databasePlaylists.map((playlist) => (playlist.playlistUri));
+
+                                return (databasePlaylistUris.includes(playlist.uri)) ? null : <Playlist key={index} addPlaylist={addPlaylistToDatabase} p={playlist} index={index} />
+                            })}
+                        </tbody>
+                    </table>
 
             </div>
         </>
---
+++ client/src/Home/UserPlaylist.js
@@ -0,0 +1,8 @@
+function UserPlaylist(props) {
+    return (
+        <>
+            <option value={props.name}></option>
+        </>
+    )
+}
+export default UserPlaylist;
