3dc96743c26e5bad5b64dc7e5383a76bfdf49202
Jasper <jskinnersloan@gmail.com>
2022-10-18 15:04:47 -0500

    admin restrictions

--- client/src/App.js
+++ client/src/App.js
@@ -138,7 +138,8 @@ function App() {
                 <Summary></Summary>
               </Route>
               <Route exact path="/admin">
-                <Admin></Admin>
+              {user.hasRole("admin") ? <Admin></Admin> : <Redirect to="/home" />}
+                
               </Route>
               <Route>
                 <ErrorPage></ErrorPage>
