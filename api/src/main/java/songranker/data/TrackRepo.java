569a971934b63fbea640c46e578a08a03e2d6a16
BSimmons10 <simmonsbgup@gmail.com>
2022-10-17 19:37:39 -0500

    repo fixes

--- api/src/main/java/songranker/data/TrackRepo.java
+++ api/src/main/java/songranker/data/TrackRepo.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface TrackRepo {
     List<Track> getTracksByAppUserId(int appUserId);
 
+    List<Track> getAllTracks(int appUserId);
+
     List<Track> getTracksByPlaylistUri(String playlistUri);
 
     boolean updateTrackEloScore(Track updatedTrack);
