585bf3058cdb8938eaed01960bd250be0fddaacf
unknown <willjrschroeder@gmail.com>
2022-10-18 10:43:08 -0500

    added live updating and conditional display on manage playlists page

--- client/src/ManagePlaylists/ManagePlaylists.js
+++ client/src/ManagePlaylists/ManagePlaylists.js
@@ -2,7 +2,7 @@ import Playlist from "../Playlist/Playlist"
 import "./ManagePlaylists.css"
 import SpotifyWebApi from 'spotify-web-api-node';
 import SpotifyAuthContext from '../context/SpotifyAuthContext';
-import { useEffect, useState, useContext  } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import DatabasePlaylist from "../Playlist/DatabasePlaylists";
@@ -18,30 +18,31 @@ const spotifyApi = new SpotifyWebApi({
 function ManagePlaylists() {
 
 
-    const spotifyAuth = useContext(SpotifyAuthContext); 
+    const spotifyAuth = useContext(SpotifyAuthContext);
     const serverAuth = useContext(AuthContext);
-    
+
 
     const [playlists, setPlaylists] = useState([]);
     const [databasePlaylists, setDatabasePlaylists] = useState([])
 
 
     function getAllPlaylists() {
-        fetch( `http://localhost:8080/api/playlist/${serverAuth.user.id}`, {
-            method:"GET",
+        fetch(`http://localhost:8080/api/playlist/${serverAuth.user.id}`, {
+            method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + serverAuth.user.token
-            }})
-        .then( response => {
-            if( response.status === 200 ) {
-                return response.json();
-            } else( console.log( response ) )
-        } )
-        .then( databasePlaylists => {
-            console.log(databasePlaylists);
-            setDatabasePlaylists( databasePlaylists );
-        });
+            }
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else (console.log(response))
+            })
+            .then(databasePlaylists => {
+                console.log(databasePlaylists);
+                setDatabasePlaylists(databasePlaylists);
+            });
     }
 
 
@@ -52,55 +53,45 @@ function ManagePlaylists() {
         spotifyApi.setAccessToken(spotifyAuth.spotifyAccessToken);
 
         spotifyApi.getMe()
-        .then(function (data) {
-            return spotifyApi.getUserPlaylists(data.body.id);
-        })
-        .then(function (data) {
-            setPlaylists(data.body.items);
-        });
-        }
-        useEffect(
-            () => {
-                loadAllPlaylists();
-                getAllPlaylists();
-            },
-            []
-        )
-
-    function playGameWithPlaylist(playlist) {
-
-
+            .then(function (data) {
+                return spotifyApi.getUserPlaylists(data.body.id);
+            })
+            .then(function (data) {
+                setPlaylists(data.body.items);
+            });
     }
 
-    
+    useEffect(() => {
+        loadAllPlaylists();
+        getAllPlaylists();
+    }, []);
 
     function addPlaylistToDatabase(playlistPackage) {
-        console.log(playlistPackage);
-
-        fetch( "http://localhost:8080/api/spotify_data", {
-            method:"POST",
+        fetch("http://localhost:8080/api/spotify_data", {
+            method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + serverAuth.user.token
             },
             body: JSON.stringify(playlistPackage)
 
-        }).then( async response => {
-            if( response.status === 201 ) {
+        }).then(async response => {
+            if (response.status === 201) {
+                getAllPlaylists();
                 return response.json(); //TODO: add success handling
-            } 
-                return Promise.reject( await response.json() );
+            }
+            return Promise.reject(await response.json());
         })
-        .catch( errorList => {
-            if( errorList instanceof TypeError ){
-                console.log( "Could not connect to api.");
-            } else {
-            } //TODO: add error handling
-        });
+            .catch(errorList => {
+                if (errorList instanceof TypeError) {
+                    console.log("Could not connect to api.");
+                } else {
+                } //TODO: add error handling
+            });
 
     }
-    
-        return(<>
+
+    return (<>
         <div className="header">Manage Playlists</div>
         <div className="flex-container">
             <div>
@@ -113,9 +104,9 @@ function ManagePlaylists() {
                         </tr>
                     </thead>
                     <tbody>
-                    {databasePlaylists.map( (pd, index) => (
-                    <DatabasePlaylist key = {index} pd={pd} playGame={playGameWithPlaylist}/>
-                ))}
+                        {databasePlaylists.map((databasePlaylist, index) => (
+                            <DatabasePlaylist key={index} pd={databasePlaylist} />
+                        ))}
                     </tbody>
                 </table>
             </div>
@@ -129,18 +120,22 @@ function ManagePlaylists() {
                         </tr>
                     </thead>
                     <tbody className="tbl-body">
-                    {playlists.map( (p, index)=> (
-                    <Playlist key={index} addPlaylist = {addPlaylistToDatabase} p={p} index={index}/>
-                    )
-                )}
+                        {playlists.map((playlist, index) => {
+                            const databasePlaylistUris = databasePlaylists.map((playlist) => (playlist.playlistUri));
+                        
+                             return (databasePlaylistUris.includes(playlist.uri)) ?  null : <Playlist key={index} addPlaylist={addPlaylistToDatabase} p={playlist} index={index} />
+                            // <Playlist key={index} addPlaylist={addPlaylistToDatabase} p={playlist} index={index} />
+
+                        })
+                        }
                     </tbody>
                 </table>
 
             </div>
-        </div>    
-        </>
-        )
-    }
+        </div>
+    </>
+    )
+}
 
 
 
